83679bf225a7f7f3b1bc59e5a2211bcb75a63281
glitchCodeBot <anushar890584@gmail.com>
2025-01-25 14:49:11 +0530

    Utils custom errors and response

--- src/index.js
+++ src/index.js
@@ -9,7 +9,15 @@ dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection error", err);
+  });
 // import express from "express";
 
 // (async () => {
